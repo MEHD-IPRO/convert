bb0e3d2a4e958b199cfdbd5a828c0e3014c77fee
MEHD-IPRO <mehdi.edarqaouy@gmail.com>
2023-09-30 12:04:08 -0700

    edite

--- script.js
+++ script.js
@@ -4,31 +4,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const toCurrencySelect = document.getElementById("toCurrency");
     const convertButton = document.getElementById("convert");
     const resultElement = document.getElementById("result");
-
-    // Replace with your currency conversion logic or API calls here
-    // Example: Fetch currency rates from an API
-
+  
+    // Sample currency conversion function
+    function convertCurrency(amount, fromCurrency, toCurrency) {
+      // Replace with your currency conversion logic or API calls here
+      // Example: You can use exchange rate data to perform the conversion
+      // For simplicity, we'll just return the same amount for demonstration
+      return amount;
+    }
+  
     // Add event listener for the convert button
     convertButton.addEventListener("click", function () {
-        const amount = parseFloat(amountInput.value);
-        const fromCurrency = fromCurrencySelect.value;
-        const toCurrency = toCurrencySelect.value;
-
-        // Perform currency conversion here and update the resultElement
-        // Example: Calculate the converted amount and display it
-        const convertedAmount = convertCurrency(amount, fromCurrency, toCurrency);
-
-        resultElement.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+      const amount = parseFloat(amountInput.value);
+      const fromCurrency = fromCurrencySelect.value;
+      const toCurrency = toCurrencySelect.value;
+  
+      // Perform currency conversion here
+      const convertedAmount = convertCurrency(amount, fromCurrency, toCurrency);
+  
+      // Display the result
+      resultElement.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
     });
-
-    // You can also populate the currency options dynamically here
-    // Example: Fetch a list of currencies from an API and add options to the select elements
-});
-
-// Sample currency conversion function
-function convertCurrency(amount, fromCurrency, toCurrency) {
-    // Replace with your currency conversion logic or API calls
-    // Example: You can use exchange rate data to perform the conversion
-    // For simplicity, we'll just return the same amount for demonstration
-    return amount;
-}
+  
+    // Populate currency options dynamically (sample)
+    function populateCurrencyOptions() {
+      // Replace this with code to fetch currency options from an API or another data source
+      const currencies = ["USD", "EUR", "GBP", "JPY", "CAD"]; // Sample currency list
+  
+      // Clear existing options
+      fromCurrencySelect.innerHTML = "";
+      toCurrencySelect.innerHTML = "";
+  
+      // Add currency options to both select elements
+      currencies.forEach((currency) => {
+        const option1 = document.createElement("option");
+        option1.value = currency;
+        option1.textContent = currency;
+        fromCurrencySelect.appendChild(option1);
+  
+        const option2 = document.createElement("option");
+        option2.value = currency;
+        option2.textContent = currency;
+        toCurrencySelect.appendChild(option2);
+      });
+    }
+  
+    // Call the function to populate currency options
+    populateCurrencyOptions();
+  });
